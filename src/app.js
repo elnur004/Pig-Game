76046b675121bcc5059d2ed38e984a5347123c00
elnur004 <lnur004@gmail.com>
2020-09-15 03:55:19 +0400

    added 'click' event and eventListener to the 'Roll Button'

--- src/app.js
+++ src/app.js
@@ -1,19 +1,40 @@
-// Variables for each player, for round score, for current active player and for the dice
-var scores, roundScore, activePlayer, dice;
+// Variables for each player, for round score and for current active player
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;  // get random number between 1 and 6
-//console.log(dice);
-
-// Add the dice number to the current player's round score;
-document.querySelector('#current-' + activePlayer).textContent = dice;
-
 // Hide the dice when beginning of the game
 document.querySelector('.dice').style.display = 'none';
 
+// Set all initial scores to '0'
+document.getElementById('score-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // 1. Random number
+    var dice, diceDom;
+    dice = Math.floor(Math.random() * 6) + 1;  // get random number between 1 and 6
+    
+    // 2. Display the result
+    diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'img/dice-' + dice + '.png';  // dynamically changing the dice image referring to the dice number
+    
+    // 3. Update theround score IF the rolled number was NOT a 1
+    
+
+})
 
 
 
+
+
+
+
+// Add the dice number to the current player's round score;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
