0d81a67438f741eac02724e415a42ca1c64c63fc
elnur004 <lnur004@gmail.com>
2020-09-17 03:48:26 +0400

    added input field, set winning score and stiled the field

--- src/challanges.js
+++ src/challanges.js
@@ -57,8 +57,20 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     // Update the UI
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+    // Adding an input value to the input field
+    var input = document.getElementById('input').value;
+    var winningScore;
+
+    // Undifined, 0, null or '' are COERCED to false
+    // Anything else is COERCED to true 
+    if(input) {
+        winningScore = input;
+    } else {
+        winningScore = 100;
+    }
+
     // Check if player won the game
-    if(scores[activePlayer] >= 100) {
+    if(scores[activePlayer] >= winningScore) {
         document.querySelector('.dice').style.display = 'none';
         document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
